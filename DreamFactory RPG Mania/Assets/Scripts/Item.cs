e3ebc08f310006576fa8a1abf0b8043a4170fd4b
leandroecorrea <leandrocorrea1989@hotmail.com>
2022-06-14 14:09:50 -0300

    initial inventory UI

---
+++ DreamFactory RPG Mania/Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public ItemData data;
+    public int amount { get; set; }
+    private readonly int maxAmount = 99;
+    private readonly int minAmount = 0;
+
+    public Item(ItemData data)=>
+        this.data = data;
+    
+    public void Add() =>
+        amount = Mathf.Min(amount + 1, maxAmount);
+    
+    public void Substract() =>    
+        amount = Mathf.Max(amount - 1, minAmount);        
+}
