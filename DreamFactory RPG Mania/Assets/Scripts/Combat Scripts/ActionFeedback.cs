0d2acab44401c5ea48f19e5f012cec86e87a14f6
leandroecorrea <leandrocorrea1989@hotmail.com>
2022-06-22 18:43:45 -0300

    lerp color of text on damage feedback method not implemented

--- DreamFactory RPG Mania/Assets/Scripts/Combat Scripts/ActionFeedback.cs
+++ DreamFactory RPG Mania/Assets/Scripts/Combat Scripts/ActionFeedback.cs
@@ -11,15 +11,29 @@ public class ActionFeedback : MonoBehaviour
     void Start()
     {
         LeanTween.scale(gameObject, new Vector3(1.1f, 1.1f, 1.1f), 0.5f);
-        transform.LeanMove(transform.position + new Vector3(0, 10f), 1f)
+        transform.LeanMove(transform.position + new Vector3(0, 10f), 1.5f)
                  .setEasePunch()
-                 .setLoopOnce();       
+                 .setLoopOnce();        
     }
 
-    private void SetColor(Color c)
+    private IEnumerator LerpTextColor(Color to, float transitionDuration)
     {
-        text.color = c;
+        float elapsedTime = 0f;
+        while(elapsedTime < transitionDuration)
+        {
+            yield return null;
+            var r = Mathf.Lerp(text.color.r, to.r, transitionDuration);
+            var g = Mathf.Lerp(text.color.g, to.g, transitionDuration);
+            var b = Mathf.Lerp(text.color.b, to.b, transitionDuration);
+            var tempColor = new Color(r, g, b);
+            text.color = tempColor;
+            elapsedTime += Time.deltaTime;
+            Debug.Log($"{r}, {g}, {b}");
+        }
+
     }
+
+
     private void Update()
     {
         if (!LeanTween.isTweening())
